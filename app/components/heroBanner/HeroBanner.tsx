b6e908e5302e684db279f624fa9baea872ee6ac5
SufyanAli56 <sufyanalii1127@gmail.com>
2025-07-03 23:59:53 -0700

    new file:   app/components/heroBanner/HeroBanner.tsx 	modified:   app/menu/page.tsx

---
+++ app/components/heroBanner/HeroBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className='bg-rose-200 w-full h-[60vh]'>
+       <div>
+
+       </div>
+    </div>
+  )
+}
+
+export default HeroBanner
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroBanner from '../components/heroBanner/HeroBanner'
 
 const page = () => {
   return (
     <div>
-
+        <HeroBanner/>
     </div>
   )
 }
